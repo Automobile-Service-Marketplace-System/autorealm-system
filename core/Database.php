15f893e88cc23f2aac6f9cad85c956cdd1987edb
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-11-11 18:27:13 +0530

    feat: add database class

---
+++ core/Database.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\core;
+
+class Database
+{
+    public \PDO $pdo;
+
+    public function __construct()
+    {
+        $dsn = $_ENV['DB_DSN'] ?? '';
+        $user = $_ENV['DB_USER'] ?? '';
+        $password = $_ENV['DB_PASSWORD'] ?? '';
+
+        $this->pdo = new \PDO($dsn, $user, $password);
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+}
