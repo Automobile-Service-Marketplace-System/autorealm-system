a77008b3d440161120149e01fca4e34c3cbd5b81
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-11-20 06:17:28 +0530

    perf: change auto reloading it won't refresh the browser with css, but reevalutae on the fly

--- public/__DEV__/ar.js
+++ public/__DEV__/ar.js
@@ -1,45 +1,63 @@
-import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+import {io} from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
+
+const styleTag = document.querySelector("style#dev");
+
+console.log(styleTag);
 
 const socket = io("ws://localhost:3001");
 
 const logSuccess = (arg) =>
-  console.log(
-    `%c[AR]: %c${arg}`,
-    "color: rgb(0, 197, 138);font-weight: bold;",
-    "font-weight: thin;"
-  );
+    console.log(
+        `%c[AR]: %c${arg}`,
+        "color: rgb(0, 197, 138);font-weight: bold;",
+        "font-weight: thin;"
+    );
 
 const logWarning = (arg) =>
-  console.log(
-    `%c[AR]: %c${arg}`,
-    "color: rgb(250, 187, 65);font-weight: bold;",
-    "font-weight: thin;"
-  );
+    console.log(
+        `%c[AR]: %c${arg}`,
+        "color: rgb(250, 187, 65);font-weight: bold;",
+        "font-weight: thin;"
+    );
 
 const logError = (arg) =>
-  console.log(
-    `%c[AR]: %c${arg}`,
-    "color: rgb(229, 48, 48);font-weight: bold;",
-    "font-weight: thin;"
-  );
+    console.log(
+        `%c[AR]: %c${arg}`,
+        "color: rgb(229, 48, 48);font-weight: bold;",
+        "font-weight: thin;"
+    );
 
 logWarning("Attempting to connect ...");
 
 let error = false;
 
 socket.on("connect", () => {
-  if (error) {
-    location.reload();
-  }
-  logSuccess("Connected to socket server");
+    if (error) {
+        location.reload();
+    }
+    logSuccess("Connected to socket server");
 });
 
 socket.on("disconnect", () => {
-  logError("Disconnected from socket server");
-  error = true;
+    logError("Disconnected from socket server");
+    error = true;
+});
+
+let lastCssUpdate = 0;
+
+socket.on("css-reload", (arg) => {
+    const now = Date.now();
+    if (now - lastCssUpdate >= 1000) {
+        lastCssUpdate = now;
+        styleTag.innerHTML = arg;
+        logSuccess("CSS reloaded");
+    }
 });
 
 socket.on("browser-reload", () => {
-  logWarning("Change detected, reloading...");
-  window.location.reload();
+    const now = Date.now();
+    if (now - lastCssUpdate >= 1000) {
+    logWarning("Change detected, reloading...");
+        window.location.reload();
+    }
 });
