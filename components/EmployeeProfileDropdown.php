ce5b96a92ebe88d53bc63c055fe7ff983db1a3d7
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-12-13 12:52:26 +0530

    fix: remove incorrect redirect when login invalid in employee login portal

--- components/EmployeeProfileDropdown.php
+++ components/EmployeeProfileDropdown.php
@@ -14,43 +14,49 @@ class EmployeeProfileDropdown
 {
     public static function render(int $employeeId, string $employeeType, string $role, int $id): void
     {
-        $employeeModel = null;
         $employee = null;
+        $profileLink = null;
         switch ($employeeType) {
             case "foreman":
                 $employeeModel = new Foreman();
                 $employee = $employeeModel->getForemanById($employeeId);
+                $profileLink = "/foreman-dashboard/profile";
                 break;
             case "security_officer":
                 $employeeModel = new SecurityOfficer();
                 $employee = $employeeModel->getSecurityOfficerById($employeeId);
+                $profileLink = "/security-officer-dashboard/profile";
                 break;
             case "technician":
                 $employeeModel = new Technician();
                 $employee = $employeeModel->getTechnicianById($employeeId);
+                $profileLink = "/technician-dashboard/profile";
                 break;
             case "admin":
                 $employeeModel = new Admin();
                 $employee = $employeeModel->getAdminById($employeeId);
+                $profileLink = "/admin-dashboard/profile";
                 break;
             case "office_staff":
                 $employeeModel = new OfficeStaff();
                 $employee = $employeeModel->getOfficeStaffById($employeeId);
+                $profileLink = "/office-staff-dashboard/profile";
                 break;
             case "stock_manager":
                 $employeeModel = new StockManager();
                 $employee = $employeeModel->getStockManagerById($employeeId);
+                $profileLink = "/stock-manager-dashboard/profile";
                 break;
         }
 
         echo "<div class='employee-profile-dropdown__toggle no_highlights' id='employee-profile-dropdown-$id'>
-                <img src='{$employee->image}' alt=\"{$employee->f_name} {$employee->l_name}'s profile photo\">
+                <img src='$employee->image' alt=\"$employee->f_name $employee->l_name's profile photo\">
                 <p>
                     {$employee->f_name[0]}. {$employee->l_name}
                 </p>
                 <i class='fa-solid fa-chevron-down'></i>
                 <nav class='employee-profile-dropdown__box' id='employee-profile-dropdown__box-$id'>
-                                <a href='/dashboard/profile'>
+                                <a href='$profileLink'>
                                     <i class='fa-solid fa-user'></i>
                                     My profile
                                 </a>
