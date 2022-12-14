ea964588009874fbf3910a1e99fae8c8d08023af
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2022-12-15 05:16:05 +0530

    feat: add ui of suggesting products and services

--- views/foreman-dashboard-view-job.php
+++ views/foreman-dashboard-view-job.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var string $jobId
+ * @var array $suggestions
  */
 ?>
 
@@ -36,6 +37,38 @@
     </li>
 </ul>
 
-<p class="new-job-notice">
+<?php
+
+if (empty($suggestions)) {
+    echo "<p class='new-job-notice'>
     Create an inspection report <br> in order to assign <br> services, products and technicians <br> to start this job
-</p>
+    </p>";
+} else {
+//    echo "<pre>";
+//    var_dump($suggestions);
+//    echo "</pre>";
+
+
+    echo "<h2 class='suggestions-heading'><i class='fa-solid fa-wrench'></i> Service suggestions based on the inspection report</h2>";
+    echo "<div class='service-suggestions'>";
+    foreach ($suggestions['services'] as $service) {
+        echo "<div class='service-suggestion'>";
+        echo "<p>$service</p>";
+        echo "<button><i class='fa-solid fa-xmark'></i></button>";
+        echo "</div>";
+    }
+    echo "</div>";
+
+    echo "<h2 class='suggestions-heading'><i class='fa-solid fa-box'></i>Product suggestions based on the inspection report</h2>";
+    echo "<div class='product-suggestions'>";
+    foreach ($suggestions['products'] as $product) {
+        echo "<div class='product-suggestion'>";
+        echo "<img src='https://ultimatestyling.co.uk/storage/category-images/headlights-2.jpg' alt='$product'>";
+        echo "<p>$product</p>";
+        echo "<button><i class='fa-solid fa-xmark'></i></button>";
+        echo "</div>";
+    }
+    echo "</div>";
+}
+?>
+
