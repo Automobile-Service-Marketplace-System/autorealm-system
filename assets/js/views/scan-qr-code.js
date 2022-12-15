42494d80567f7fc2929fb4e7b2f45b03def8683f
Tharu3 <2020cs065@stu.ucsc.cmb.ac.lk>
2022-12-15 14:28:47 +0530

    feat: add scan qr code functionality

--- assets/js/views/scan-qr-code.js
+++ assets/js/views/scan-qr-code.js
@@ -1,15 +1,50 @@
-import {Html5QrcodeScanner} from 'html5-qrcode'; 
+import QRScanner from 'qr-scanner';
+import Notifier from "../components/Notifier";
 
-const qrcodeScanner=new Html5QrcodeScanner("reader" /*ID from .html file*/,{
-    fps: 10,
-    aspectRatio: 1.0,
-    showZoomSliderIfSupported:true,
-});
+const openCameraBtn = document.querySelector("#open-camera-btn");
+const imageInput = document.querySelector("#image-input");
+const resultContainer = document.querySelector("#qrcode-result");
+const qrCodeDate = document.querySelector("#qrcode-result p:first-child");
+const qrCodeTime = document.querySelector("#qrcode-result p:nth-child(2)");
+const qrCodeRegisterNo = document.querySelector("#qrcode-result p:nth-child(3)");
 
-qrcodeScanner.render((arg1,arg2)=>{
-    let qrcode_result = document.querySelector(".qrcode-result");
-    qrcode_result.style.display = "block";
-    console.log(arg1,arg2)
-}, (errMsg,err)=>{
-    console.log(errMsg,err)
-})
+const reader = document.querySelector("#reader")
+
+if (reader && qrCodeDate && qrCodeRegisterNo && qrCodeTime && resultContainer) {
+    reader?.addEventListener("click", () => {
+        imageInput.click();
+    })
+
+    imageInput?.addEventListener("change", async (e) => {
+        const files = e.target.files;
+        const image = files[0];
+        console.log(image);
+        const dataReader = new FileReader();
+        dataReader.readAsDataURL(image);
+        dataReader.onload = (e) => {
+            reader.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${e.target.result})`;
+            const cameraIcon = document.querySelector("#reader svg")
+            cameraIcon.style.color = "var(--color-white)";
+        }
+
+        try {
+            console.log("start scanning");
+            const scanResult = await QRScanner.scanImage(image)
+            const appointmentObject = JSON.parse(scanResult);
+            qrCodeDate.innerHTML = `<strong>Appointment Date</strong><span>${appointmentObject['date']}</span>`;
+            qrCodeTime.innerHTML = `<strong>Time Slot</strong><span>${appointmentObject['timeslot']}</span>`;
+            qrCodeRegisterNo.innerHTML = `<strong>Register Number</strong><span>${appointmentObject['registrationNumber']}</span>`;
+            resultContainer.style.display = "flex";
+
+            Notifier.show({
+                header: "Scan Successful",
+                text: "QR Code is valid.<br>Approve and create the admitting report",
+                type: "success"
+            })
+        } catch (e) {
+            console.log(e);
+        }
+
+    })
+
+}
