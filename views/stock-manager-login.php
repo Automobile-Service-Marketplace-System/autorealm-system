ddcba12bb27d10ab1a986cc0b8d17c5e8392d684
Avishka <baavishka@gmail.com>
2022-12-20 23:12:00 +0530

    fix

--- views/stock-manager-login.php
+++ views/stock-manager-login.php
@@ -28,11 +28,10 @@
 
     }
 
-    .stockmanager-login{
+    .stock-manager-login{
         background-color:white;
        /*border: 1px solid;*/
         width: 30%;
-        padding: 2rem;
         box-shadow: 0 0.2rem 1.5rem rgba(var(--black), 0.1);
         gap: 1.5rem;
         padding: 1rem;
@@ -78,7 +77,7 @@
 
 
 <div class="form-container">
-    <form class="stockmanager-login" action="/stockmanager-login" method="post">
+    <form class="stock-manager-login" action="/stockmanager-login" method="post">
         <div class="stock-sign-heading">Stock Manager Signup</div>
         <div class="input-container">
             <label for="email">Email</label>
