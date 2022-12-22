be974f9d14b1b12b73b288e016039c36a28de86c
Avishka <baavishka@gmail.com>
2022-12-22 13:34:20 +0530

    feat:make add suppliers modal:

--- assets/js/views/add-suppliers.js
+++ assets/js/views/add-suppliers.js
@@ -5,24 +5,56 @@ const addSupplierBtn = document.querySelector('#add-supplier-btn');
 
 addSupplierBtn?.addEventListener('click', () => {
 
-    const template = `<div>
+    const addSupplier = `<div>
             <button class="modal-close-btn">
                 <i class="fas fa-times"></i>
             </button>
-            <p>Hello, I'm a modal</p>
+            <form action="/stock-manager-dashboard/suppliers" method="post" class="stock-manager-add-supplier-form" enctype="multipart/form-data">
+            <h1>Supplier Details</h1>
+            
+            <div class="form-item">
+                <label for='name'>Supplier Name.<sup>*</sup></label>
+                <input type='text' name='name' id='name' placeholder='' required  value=''   >
+            </div>
+            
+            <div class="form-item">
+                <label for='company_reg_no'>Business Registration No.<sup>*</sup></label>
+                <input type='text' name='company_reg_no' id='company_reg_no' placeholder='' required  value=''   >
+            </div>
+            
+            <div class="form-item">
+                <label for='email'>Email.<sup>*</sup></label>
+                <input type='text' name='email' id='email' placeholder='' required  value=''   >
+            </div>
+            
+            <div class="form-item">
+                <label for='contact_no'>Mobile Number<sup>*</sup></label>
+                <input type='text' name='contact_no' id="contact_no" placeholder='' required  value=''   >
+            <div>
+       
+            <div class="form-item">
+                <label for='address'>Address.<sup>*</sup></label>  
+                <input type='text' name='address' id='address' placeholder='' required  value=''   >
+            </div>
+            
+            <div class="form-item">
+                <label for='sales_manager'>Sales Manager Name.<sup>*</sup></label>
+                <input type='text' name='sales_manager' id='sales_manager' placeholder='' required  value=''   >
+            
+            <div class="form-container"></div>
             <button class="btn" id="open-another">Open another modal</button>
            </div>`
-    console.log(template)
-    const element = htmlToElement(template);
-    console.log(element)
-    const openAnotherBtn = element.querySelector('#open-another');
-    openAnotherBtn?.addEventListener('click', () => {
+    //console.log(template)
+    const element = htmlToElement(addSupplier);
+    //console.log(element)
+    //const openAnotherBtn = element.querySelector('#open-another');
+   // openAnotherBtn?.addEventListener('click', () => {
        Modal.show({
+           key: "add-supplier",
            closable: true,
-           content:  `<div>This is another modal</div>`,
-           key: 'another-modal'
+           content: element
        })
-    } )})
+    } )
 // import {Modal} from "../components/Modal"
 
 //     Modal.show({
