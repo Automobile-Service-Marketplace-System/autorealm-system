5e2257a0040b139c5da07fbf853a1b60cdef8da3
Tharu3 <2020cs065@stu.ucsc.cmb.ac.lk>
2023-01-13 20:55:13 +0530

    Practice

--- models/SecurityOfficer.php
+++ models/SecurityOfficer.php
@@ -1,53 +1,53 @@
 <?php
 
-namespace app\models;
+// namespace app\models;
 
-use app\core\Database;
+// use app\core\Database;
 
-class SecurityOfficer
-{
-    private \PDO $pdo;
-    private array $body;
+// class SecurityOfficer
+// {
+//     private \PDO $pdo;
+//     private array $body;
 
-    public function __construct(array $registerBody = [])
-    {
-        $this->pdo = Database::getInstance()->pdo;
-        $this->body = $registerBody;
-    }
+//     public function __construct(array $registerBody = [])
+//     {
+//         $this->pdo = Database::getInstance()->pdo;
+//         $this->body = $registerBody;
+//     }
 
-    public function getSecurityOfficerById(int $securityOfficer_id): bool|object
-    {
-        $stmt = $this->pdo->prepare("SELECT * FROM employee e INNER JOIN securityOfficer s on e.employee_id = s.employee_id  WHERE e.employee_id = :employee_id");
-        $stmt->execute([
-            ":employee_id" => $securityOfficer_id
-        ]);
-        return $stmt->fetchObject();
-    }
+//     public function getSecurityOfficerById(int $securityOfficer_id): bool|object
+//     {
+//         $stmt = $this->pdo->prepare("SELECT * FROM employee e INNER JOIN securityOfficer s on e.employee_id = s.employee_id  WHERE e.employee_id = :employee_id");
+//         $stmt->execute([
+//             ":employee_id" => $securityOfficer_id
+//         ]);
+//         return $stmt->fetchObject();
+//     }
 
-    public function login(): array |object
-    {
-        $errors = [];
-        $employee = null;
+//     public function login(): array |object
+//     {
+//         $errors = [];
+//         $employee = null;
 
-        if (!filter_var($this->body['email'], FILTER_VALIDATE_EMAIL)) {
-            $errors['email'] = 'Email must be a valid email address';
-        } else {
-            $query = "SELECT * FROM employee WHERE email = :email";
-            $statement = $this->pdo->prepare($query);
-            $statement->bindValue(':email', $this->body['email']);
-            $statement->execute();
-            $employee = $statement->fetchObject();
-            if (!$employee) {
-                $errors['email'] = 'Email does not exist';
-            } else {
-                if (!password_verify($this->body['password'], $employee->password)) {
-                    $errors['password'] = 'Password is incorrect';
-                }
-            }
-        }
-        if (empty($errors)) {
-            return $employee;
-        }
-        return $errors;
-    }
-}
+//         if (!filter_var($this->body['email'], FILTER_VALIDATE_EMAIL)) {
+//             $errors['email'] = 'Email must be a valid email address';
+//         } else {
+//             $query = "SELECT * FROM employee WHERE email = :email";
+//             $statement = $this->pdo->prepare($query);
+//             $statement->bindValue(':email', $this->body['email']);
+//             $statement->execute();
+//             $employee = $statement->fetchObject();
+//             if (!$employee) {
+//                 $errors['email'] = 'Email does not exist';
+//             } else {
+//                 if (!password_verify($this->body['password'], $employee->password)) {
+//                     $errors['password'] = 'Password is incorrect';
+//                 }
+//             }
+//         }
+//         if (empty($errors)) {
+//             return $employee;
+//         }
+//         return $errors;
+//     }
+// }
--- public/index.php
+++ public/index.php
@@ -103,10 +103,10 @@ $app->router->get("/office-staff-dashboard/vehicles/by-customer", [VehiclesContr
 $app->router->get("/office-staff-dashboard/vehicles/add/by-customer", [VehiclesController::class, 'getAddVehiclePage']);
 $app->router->post("/office-staff-dashboard/vehicles/add/by-customer", [VehiclesController::class, 'addVehicle']);
 
-
+  
 //security officer roots
-$app->router->get( "/security-officer-login", [AuthenticationController::class,'getSecurityOfficerLoginPage']);
-$app->router->post( "/security-officer-login", [AuthenticationController::class,'loginSecurityOfficer']);
+// $app->router->get( "/security-officer-login", [AuthenticationController::class,'getSecurityOfficerLoginPage']);
+// $app->router->post( "/security-officer-login", [AuthenticationController::class,'loginSecurityOfficer']);
 $app->router->get( "/security-officer-dashboard/profile", [DashboardController::class,'getSecurityOfficerDashboardProfile']);
 $app->router->get("/security-officer-dashboard/check-appointment", [AppointmentController::class, 'getAppointmentPage']);
 
