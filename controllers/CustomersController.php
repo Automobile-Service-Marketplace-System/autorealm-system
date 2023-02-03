673031c037402cdb615250042f6d57d5e0e949d9
Tharu3 <2020cs065@stu.ucsc.cmb.ac.lk>
2023-02-03 15:38:00 +0530

    fix: change admin dashboard with linked product,customer,vehicle,supplier

--- controllers/CustomersController.php
+++ controllers/CustomersController.php
@@ -13,18 +13,30 @@ class CustomersController
     public function getCustomersPage(Request $req, Response $res): string
     {
 
-        if ($req->session->get("is_authenticated") && $req->session->get("user_role") === "office_staff_member") {
+        if ($req->session->get("is_authenticated") && ($req->session->get("user_role") === "office_staff_member" || $req->session->get("user_role") === "admin")) {
 
             $customerModel = new Customer();
             $customers = $customerModel->getCustomers();
 
-            return $res->render(view:"office-staff-dashboard-customers-page", layout:"office-staff-dashboard",
-                pageParams:["customers" => $customers],
-                layoutParams:[
-                    'title' => 'Customers',
-                    'pageMainHeading' => 'Customers',
-                    'officeStaffId' => $req->session->get('user_id'),
+            if($req->session->get("user_role") === "office_staff_member"){
+                return $res->render(view:"office-staff-dashboard-customers-page", layout:"office-staff-dashboard",
+                    pageParams:["customers" => $customers],
+                    layoutParams:[
+                        'title' => 'Customers',
+                        'pageMainHeading' => 'Customers',
+                        'officeStaffId' => $req->session->get('user_id'),
                 ]);
+            }
+
+            if($req->session->get("user_role") === "admin"){
+                return $res->render(view:"office-staff-dashboard-customers-page", layout:"admin-dashboard",
+                    pageParams:["customers" => $customers],
+                    layoutParams:[
+                        'title' => 'Customers',
+                        'pageMainHeading' => 'Customers',
+                        'employeeId' => $req->session->get('user_id'),
+                ]);
+            }
         }
 
         return $res->redirect(path:"/login");
--- controllers/SuppliersController.php
+++ controllers/SuppliersController.php
@@ -10,20 +10,34 @@ class SuppliersController
 {
     public function getSuppliersPage(Request $req, Response $res): string
     {
-        if($req->session->get("is_authenticated") && $req->session->get("user_role") === "stock_manager"){
+        if($req->session->get("is_authenticated") && ($req->session->get("user_role") === "stock_manager" || $req->session->get("user_role") === "admin")){
 
             $supplierModel = new Supplier();
             $suppliers = $supplierModel->getSuppliersList();
 
-            return $res->render(view: "stock-manager-dashboard-view-suppliers", layout: "stock-manager-dashboard",
-                pageParams: [
-                'suppliers' => $suppliers
-                ],
-                layoutParams: [
-                'title' => 'Suppliers',
-                'pageMainHeading' => 'Suppliers',
-                'employeeId' => $req->session->get('user_id')
-            ]);
+            if($req->session->get("user_role") === "stock_manager"){
+                return $res->render(view: "stock-manager-dashboard-view-suppliers", layout: "stock-manager-dashboard",
+                    pageParams: [
+                    'suppliers' => $suppliers
+                    ],
+                    layoutParams: [
+                    'title' => 'Suppliers',
+                    'pageMainHeading' => 'Suppliers',
+                    'employeeId' => $req->session->get('user_id')
+                ]);
+            }
+
+            if($req->session->get("user_role") === "admin"){
+                return $res->render(view: "stock-manager-dashboard-view-suppliers", layout: "admin-dashboard",
+                    pageParams: [
+                    'suppliers' => $suppliers
+                    ],
+                    layoutParams: [
+                    'title' => 'Suppliers',
+                    'pageMainHeading' => 'Suppliers',
+                    'employeeId' => $req->session->get('user_id')
+                ]);
+            }
         }
 
         return $res->redirect(path: "/login");
