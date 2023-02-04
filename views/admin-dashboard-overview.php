4170548e06db66cec494505a44c408fa8f70eec5
Tharu3 <2020cs065@stu.ucsc.cmb.ac.lk>
2023-02-04 10:48:48 +0530

    fix: create button for the genarate report

--- views/admin-dashboard-overview.php
+++ views/admin-dashboard-overview.php
@@ -1,3 +1,13 @@
+<style>
+    #btn3{
+        color: white;
+        background-color: mediumseagreen;
+        border-radius: 100px;
+        height: 1cm;
+        width: 4cm;
+    }
+</style>
+
 <h>Here’s analytics for the month of August, 2022</h>
 <div>
     <button type="button" id="btn3">Generate report</button>
