4b31fc7070a81b5181ebf4ed12d25d688b6a5a67
Tharu3 <2020cs065@stu.ucsc.cmb.ac.lk>
2023-02-07 11:59:52 +0530

    fix: admin-overview page

--- views/admin-dashboard-overview.php
+++ views/admin-dashboard-overview.php
@@ -1,14 +1,18 @@
-<style>
-    #btn3{
-        color: white;
-        background-color: mediumseagreen;
-        border-radius: 100px;
-        height: 1cm;
-        width: 4cm;
-    }
-</style>
-
 <h>Here’s analytics for the month of August, 2022</h>
-<div>
-    <button type="button" id="btn3">Generate report</button>
+<div class="Chart-Grid">
+    <div class="image-card">
+        <img src="images/placeholders/chart1.png">
+    </div>
+    <div class="image-card">
+        <img src="images/placeholders/chart2.png">
+    </div>
+    <div class="image-card">
+        <img src="images/placeholders/chart3.png">
+    </div>
+    <div class="image-card">
+        <img src="images/placeholders/chart4.png">
+    </div>
+</div>
+<div class="btn3">
+    <button type="button">Generate report</button>
 </div>
