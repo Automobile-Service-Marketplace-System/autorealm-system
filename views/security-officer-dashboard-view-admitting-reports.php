bf8dd26cf95b9d7cd8f8d2e17d5fa61e5b6d712c
Tharu3 <2020cs065@stu.ucsc.cmb.ac.lk>
2023-02-09 11:10:00 +0530

    Fix: view admitting reports

---
+++ views/security-officer-dashboard-view-admitting-reports.php
@@ -0,0 +1,22 @@
+<div class="admitting-reports">
+    <div class="appointment3">
+        <p><b>Name: </b>Nethsara Sandeepa</p>
+        <br><b>Registrain Number: </b>ACN 6534</p>
+        <br><b>Date: </b>Today</p>
+    <div>
+    <div class="appointment1">
+        <p><b>Name: </b>Supun Dilshan</p>
+        <br><b>Registrain Number: </b>BCM 4576</p>
+        <br><b>Date: </b>2023-04-21</p>
+    <div>
+    <div class="appointment4">
+        <p><b>Name: </b>Avishka Sathyanjana</p>
+        <br><b>Registrain Number: </b>BMC 6078</p>
+        <br><b>Date: </b>Today</p>
+    <div>
+    <div class="appointment2">
+        <p><b>Name: </b>Tharushi Chethana</p>
+        <br><b>Registrain Number: </b>AQW 4567</p>
+        <br><b>Date: </b>2023-02-15</p>
+    <div>
+</div>
