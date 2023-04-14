a0d53f210bd59a30a1e05dd7d4714b4fc98c010d
Avishka <baavishka@gmail.com>
2023-04-14 15:49:36 +0530

    feat: add styles for the status columns

--- views/stock-manager-dashboard-view-orders.php
+++ views/stock-manager-dashboard-view-orders.php
@@ -3,6 +3,7 @@
 /**
  * @var array $orders
  */
+//var_dump($orders);
 
 use app\components\Table;
 
@@ -14,21 +15,39 @@ foreach ($orders as $order){
     if($order["Status"] === "Paid"){
         $order["Status"] = "Not Prepared";
     }
+    $statusElements = "";
+        switch ($order["Status"]) {
+            case "Not Prepared":
+                $statusElements = "<span class='status-btn-shape ntprep-st-col'>{$order["Status"]}</span>";
+                break;
+            case "Prepared":
+                $statusElements = "<span class='status-btn-shape prep-st-col'>{$order["Status"]}</span>";
+                break;
+            case "Delivery":
+                $statusElements = "<span class='status-btn-shape del-st-col'>{$order["Status"]}</span>";
+                break;
+            case "CourierConfirmed":
+                $statusElements = "<span class='status-btn-shape cur-st-col'>{$order["Status"]}</span>";
+                break;
+            case "CustomerConfirmed":
+                $statusElements = "<span class='status-btn-shape cus-st-col'>{$order["Status"]}</span>";
+                break;
+        }
     $items[]=[
         "ID" => $order["ID"],
         "Customer Name" => $order["Customer Name"],
         "Shipping Address" => $order["Shipping Address"],
         "Order Date" => $order["Order Date"],
         "Payment Amount" => number_format($paymentAmount,2, '.', ','),
-        "Status" => $order["Status"],
+        "Status" => $statusElements,
 
         " " => "<div style='display: flex;align-items: center;justify-content: center;gap: 1rem;padding-inline: 0.25rem'>
-                            <a href='/orders/view?id={$order["ID"]}' class='btn btn--rounded'>
+                            <a href='/orders/view?id={$order["ID"]}' class='btn btn--info btn--rounded'>
                                 <i class='fa-solid fa-arrow-up-right-from-square'></i>
                             </a>
                       </div>"
     ];
 }
 
-Table::render(items: $items, columns: $columns, keyColumns: ["ID", "Status"]);
+Table::render(items: $items, columns: $columns, keyColumns: ["ID", " "]);
 
