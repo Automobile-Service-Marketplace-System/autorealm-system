aa5282ecaf087fc4e17b4d2b85d529e14e81626d
Avishka <baavishka@gmail.com>
2023-04-27 04:14:13 +0530

    feat:add update supplier validation

--- models/Supplier.php
+++ models/Supplier.php
@@ -130,8 +130,8 @@ class Supplier
     public function updateSupplier(): bool|array|string
     {
         //check for the errors
-        //$errors = $this->validateAddSupplierForm();
-        $errors = [];
+        $errors = $this->validateUpdateSupplier();
+//        $errors = [];
 
         if (empty($errors)) {
             $query = "UPDATE supplier SET
@@ -269,6 +269,59 @@ class Supplier
             if ($statement2->rowCount() > 0) {
                 $errors['contact_no_2'] = 'Contact Number already in use.';
             }
+        }
+
+        return $errors;
+    }
+
+    private function validateUpdateSupplier(): array
+    {
+        $errors = [];
+
+        if (trim($this->body["name"]) === "") {
+            $errors["name"] = "Name is required";
+        } else {
+            $query = "SELECT * FROM supplier WHERE name = :name AND name != :old_name";
+            $statement = $this->pdo->prepare($query);
+
+            $statement->bindValue(":name", $this->body["name"]);
+            $statement->bindValue(":old_name", $this->body["old_name"]);
+            $statement->execute();
+
+            if ($statement->rowCount() > 0) {
+                $errors['name'] = 'Supplier Name already in use.';
+            }
+        }
+
+        if (trim($this->body["company_reg_no"])==="") {
+            $errors["company_reg_no"] = "Business reg no is required";
+        } else {
+            $query = "SELECT * FROM supplier WHERE company_reg_no = :company_reg_no AND company_reg_no != :old_company_reg_no";
+            $statement = $this->pdo->prepare($query);
+
+            $statement->bindValue(":company_reg_no", $this->body["company_reg_no"]);
+            $statement->bindValue(":old_company_reg_no", $this->body["old_company_reg_no"]);
+            $statement->execute();
+
+            if ($statement->rowCount() > 0) {
+                $errors['company_reg_no'] = 'Company Registration Number already in use.';
+            }
+        }
+
+        if (trim($this->body["email"])==="") {
+            $errors["email"] = "Email is required";
+        } else {
+            $query = "SELECT * FROM supplier WHERE email = :email AND email != :old_email";
+            $statement = $this->pdo->prepare($query);
+
+            $statement->bindValue(":email", $this->body["email"]);
+            $statement->bindValue(":old_email", $this->body["old_email"]);
+            $statement->execute();
+
+            if ($statement->rowCount() > 0) {
+                $errors['email'] = 'Email already in use.';
+            }
+
         }
 
         return $errors;
