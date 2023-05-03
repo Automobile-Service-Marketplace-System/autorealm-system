68a3243b581b50d7d380f907961904bee3e4be49
DilshanSenewirathne <dilshansenevi99@gmail.com>
2023-05-03 09:39:31 +0530

    fix: change /jobs route to /jobcards

--- controllers/JobsController.php
+++ controllers/JobsController.php
@@ -285,8 +285,8 @@ class JobsController
                     "page"=>$page
                 ],  
                 layoutParams: [
-                    'title' => 'Jobs',
-                    'pageMainHeading' => 'Jobs',
+                    'title' => 'JobCards',
+                    'pageMainHeading' => 'JobCards',
                     'officeStaffId' => $req->session->get('user_id')
             ]);
         }
