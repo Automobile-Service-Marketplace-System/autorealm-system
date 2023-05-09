974864848834b109ad57951cc464163b3de72fc4
DilshanSenewirathne <dilshansenevi99@gmail.com>
2023-05-09 14:30:35 +0530

    refactor: change vehicle reg no to vin in showing jobcard table

--- models/JobCard.php
+++ models/JobCard.php
@@ -326,7 +326,7 @@ class JobCard
                 job_card_id as 'JobCard ID',
                 concat(c.f_name,' ',c.l_name) as 'Customer Name',
                 concat(e.f_name,' ',e.l_name) as 'Employee Name',
-                v.reg_no as 'Vehicle Reg No',
+                vehicle_reg_no as 'Vehicle Reg No',
                 start_date_time as 'Start Date Time',
                 end_date_time as 'End Date Time',
                 status as 'Status'
@@ -336,8 +336,6 @@ class JobCard
                 customer c ON c.customer_id = j.customer_id
             INNER JOIN 
                 employee e ON e.employee_id = j.employee_id
-            LEFT JOIN 
-                vehicle v ON v.vin = j.vin
             ORDER BY
                 j.job_card_id $limitClause $pageClause"
         )->fetchAll(PDO::FETCH_ASSOC);
